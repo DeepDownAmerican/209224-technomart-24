5ecd2b99170cdb70f0b8f70deb9910293ed845f5
Egor Vidrin Olegovich <46793471+DeepDownAmerican@users.noreply.github.com>
2019-02-28 23:31:15 +0500

    Исправления для подачи проекта на предзащиту
    1) Исправлены ошибки в мелкой разметке.
    2) добавлены попапы.
    3) добавлены размеры картинкам.

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var link = document.querySelector(".btn-letter");
+var popup = document.querySelector(".feed-back-popup");
+var close = popup.querySelector(".btn-close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-show");
+});
+
+
+var maplink = document.querySelector(".open-map");
+var mappopup = document.querySelector(".map-popup");
+var mapclose = mappopup.querySelector(".btn-close-map");
+
+maplink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mappopup.classList.add("popup-show");
+});
+
+mapclose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mappopup.classList.remove("popup-show");
+});
+
+
+var buylink = document.querySelector(".btn-buy");
+var buypopup = document.querySelector("add-cart-popup.");
+var buyclose = buypopup.querySelector(".btn-close");
+
+buylink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buypopup.classList.add("popup-show");
+});
+
+buyclose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buypopup.classList.remove("popup-show");
+});
+
