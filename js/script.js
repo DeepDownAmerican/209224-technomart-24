479b9eb5b25d8e198984485394ff21a5620219f9
Egor Vidrin Olegovich <46793471+DeepDownAmerican@users.noreply.github.com>
2019-03-19 20:59:03 +0500

    Небольшие изменения перед отправкой на защиту

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// FIXME: работает наполовину
 var link = document.querySelector(".btn-letter");
 var popup = document.querySelector(".feed-back-popup");
 var close = popup.querySelector(".btn-close");
@@ -40,5 +42,49 @@ buyClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   buyPopup.classList.remove("popup-show");
 });
+
+// FIXME: НЕ работает
+// var buttonDelivery = document.querySelector(".btn-delivery");
+// var buttonGuarantee = document.querySelector(".btn-guarantee");
+// var buttonCredit = document.querySelector(".btn-credit");
+// var servicesDelivery = document.querySelector(".delivery");
+// var servicesGuarantee = document.querySelector(".guarantee");
+// var servicesCredit = document.querySelector(".credit");
+
+
+//   buttonDelivery.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     servicesDelivery.classList.add("services-show");
+//     servicesCredit.classList.remove("services-show");
+//     servicesGuarantee.classList.remove("services-show");
+//     buttonDelivery.classList.add("services-items-action");
+//     buttonCredit.classList.remove("services-items-action");
+//     buttonGuarantee.classList.remove("services-items-action");
+//   });
+
+//   buttonGuarantee.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     servicesGuarantee.classList.add("services-show");
+//     servicesCredit.classList.remove("services-show");
+//     servicesDelivery.classList.remove("services-show");
+//     buttonGuarantee.classList.add("services-items-action");
+//     buttonDelivery.classList.remove("services-items-action");
+//     buttonCredit.classList.remove("services-items-action");
+//   });
+
+//   buttonCredit.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     servicesCredit.classList.add("services-show");
+//     servicesGuarantee.classList.remove("services-show");
+//     servicesDelivery.classList.remove("services-show");
+//     buttonCredit.classList.add("services-items-action");
+//     buttonDelivery.classList.remove("services-items-action");
+//     buttonGuarantee.classList.remove("services-items-action");
+//   });
+
+
+
+
+
 
 
